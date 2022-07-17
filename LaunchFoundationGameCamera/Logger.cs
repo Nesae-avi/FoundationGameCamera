9ac96dee8ee05cbf2dd14b51dbdc70c51b305500
silver-eagle1 <dev@silver-eagle1>
2022-07-17 17:31:42 +0200

    Change log file name

--- LaunchFoundationGameCamera/Logger.cs
+++ LaunchFoundationGameCamera/Logger.cs
@@ -5,8 +5,8 @@ namespace LaunchFoundationGameCamera
     // Very simple logging class.
     internal class Logger
     {
-        private static string? assembly_filename => $"{Assembly.GetExecutingAssembly().GetName().Name}.txt";
-        private static string filename => assembly_filename == null ? "log.txt" : assembly_filename;
+        private static string? assembly_filename => $"{Assembly.GetExecutingAssembly().GetName().Name}";
+        private static string filename => assembly_filename == null ? "log.txt" : $"{assembly_filename}_log.txt";
 
         public static void Log(string text)
         {
